accbb8123b2c34b42ddfa8a9119166ecd4ab49a4
chenghuiyong <chenghuiyong@weipei.cc>
2016-10-14 18:46:42 +0800

    nginx docker

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop nginx
-docker rm nginx
-docker rmi bravist/nginx
+docker stop nginx-docker
+docker rm nginx-docker
+docker rmi bravist/nginx:1.0
 
 # 重新生成
-docker build -t bravist/nginx ./
+docker build -t bravist/nginx:1.0 ./
