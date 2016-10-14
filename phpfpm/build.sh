bfde030884a8f8804db09365a322ab3f98e9c433
chenghuiyong <chenghuiyong@weipei.cc>
2016-10-14 15:54:54 +0800

    php-fpm docker

--- phpfpm/build.sh
+++ phpfpm/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop php7.0.11-fpm-alpine
-docker rm php7.0.11-fpm-alpine
+docker stop php7.0.11-fpm-alpine:1.0
+docker rm php7.0.11-fpm-alpine:1.0
 docker rmi bravist/php7.0.11-fpm-alpine:1.0 
 
 # 重新生成
-docker build -t bravist/php7.0.11-fpm-alpine ./
+docker build -t bravist/php7.0.11-fpm-alpine:1.0 ./
