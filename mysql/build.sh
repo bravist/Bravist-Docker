aa5ebc6c1a3c7c7bcd5408433363c0d851bf4de6
chenghuiyong <chenghuiyong@weipei.cc>
2016-10-14 18:05:17 +0800

    mysql docker

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop mysql
-docker rm mysql
-docker rmi water/mysql
+docker stop mysql-docker
+docker rm mysql-docker
+docker rmi bravist/mysql-5.6:1.0
 
 # 重新生成
-docker build -t water/mysql ./
+docker build -t bravist/mysql-5.6:1.0 ./
--- mysql/init_mysql.sh
+++ mysql/init_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d /apps/mysql/data ]; then
+if [ -d /var/lib/mysql/data ]; then
   echo "[i] MySQL directory already present, skipping creation"
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
