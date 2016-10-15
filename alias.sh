3a59927e338078693ecdd6e8f4062c12fa86449a
会勇 <chenghuiyong1987@gmail.com>
2016-10-15 14:44:02 +0800

    Delete alias.sh

--- alias.sh
+++
@@ -1,3 +0,0 @@
-alias php="docker exec $(docker ps -a | grep php-fpm-docker | cut -d ' ' -f 1) php"
-alias composer="docker exec $(docker ps -a | grep php-fpm-dokcer | cut -d ' ' -f 1) composer"
-alias mysql="docker exec $(docker ps -a | grep mysql-docker | cut -d ' ' -f 1) mysql"
