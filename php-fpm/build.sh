bb0d855a8ddf43e0588ce3de1b32498f1964f03f
chenghuiyong <chenghuiyong@weipei.cc>
2016-11-12 19:52:08 +0800

    add git

--- php-fpm/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 清除已有的
-docker stop php-fpm-docker
-docker rm php-fpm-docker
-docker rmi bravist/php-7.0-fpm-alpine:1.0  
-
-# 重新生成
-docker build -t bravist/php-7.0-fpm-alpine:1.0 ./
